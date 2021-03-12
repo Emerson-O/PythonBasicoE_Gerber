2dab73579fa4bbc0b413e21dda06c78e9bade678
Gerber Emerson Ordoñez tucubal <gemersonordonez@gmail.com>
2021-03-12 17:02:23 -0600

    Hoja de trabajo No.3
    Gerber ordoñez python básico

---
+++ HT3.py
@@ -0,0 +1,35 @@
+#Ejercicio1
+print("EJERCICIO 1")
+con1 = input("Ingrese la contraseña a almacenar por favor: ")
+print("")
+contraseña = input("Introduce tu contraseña para ingresar: ")
+print("")
+if con1.lower() == contraseña.lower():
+    print("Contraseña correcta, Bienvenido")
+else:
+    print("La contraseña incorrecta")
+print("")
+
+
+## EJERCICIO No. 2
+print("")
+print("EJERCICIO NO. 2")
+print("=================")
+nombre = input ("¿ Cuál es su nombre?")
+genero = input("¿Cuál es tu genero?")
+
+if generon == "M":
+    if nombre.lower() < "m":
+        group = "A"
+    else:
+            group = "B"
+else:
+     if nombre.lower () > "n":
+         group = "A"
+     else:
+         group = "B"
+print("Tu grupo es"+group)
+print("Gracias por usar el programa")                    
+    
+
+
